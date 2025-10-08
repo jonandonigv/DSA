5ec0b351b495991204c3aa9d0b3f4e66dea7db27
jonandonigv <jonandonigv@gmail.com>
2025-10-08 17:24:30 +0200

    feature: Doubly linked list

--- src/doubly_linked_list.rs
+++ src/doubly_linked_list.rs
@@ -11,16 +11,57 @@ struct Node<T> {
 }
 
 #[derive(Debug)]
-struct Doubly_linked_list<T> {
+pub struct Doubly_linked_list<T> {
     head: Option<Rc<RefCell<Node<T>>>>,
     tail: Option<Rc<RefCell<Node<T>>>>,
 }
 
-impl<T> Doubly_linked_list<T> {
-    fn new() -> Self {
+impl<T: std::fmt::Debug> Doubly_linked_list<T> {
+    pub fn new() -> Self {
         Doubly_linked_list {
             head: None,
             tail: None,
         }
+    }
+
+    pub fn push_front(&mut self, value: T) {
+        let new_node = Rc::new(RefCell::new(Node {
+            value,
+            next: self.head.clone(),
+            prev: None,
+        }));
+
+        if let Some(old_head) = self.head.take() {
+            old_head.borrow_mut().prev = Some(Rc::downgrade(&new_node));
+        } else {
+            self.tail = Some(new_node.clone());
+        }
+
+        self.head = Some(new_node);
+    }
+
+    pub fn pop_front(&mut self) -> Option<T> {
+        self.head.take().map(|old_head| {
+            self.head = old_head.borrow().next.clone();
+
+            if let Some(new_head) = &self.head {
+                new_head.borrow_mut().prev = None;
+            } else {
+                self.tail = None;
+            }
+
+            Rc::try_unwrap(old_head).ok().unwrap().into_inner().value
+        })
+    }
+
+    pub fn print_list(&self) {
+        let mut current = self.head.clone();
+        print!("List: ");
+        while let Some(node) = current {
+            print!("{:?} <-> ", node.borrow().value);
+            current = node.borrow().next.clone();
+        }
+
+        println!("None");
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use dsa::{add, linked_list::Linked_list};
+use dsa::{doubly_linked_list::Doubly_linked_list, linked_list::Linked_list};
 
 fn main() {
     // Linked list
@@ -15,7 +15,16 @@ fn main() {
     println!("Popped: {:?}", list.pop());
     println!("List after pops: {:?}", list);
 
-    let result = add(2, 2);
-    println!("Hello world!");
-    println!("{result}");
+    // Doubly linked list
+    let mut d_list: Doubly_linked_list<i32> = dsa::doubly_linked_list::Doubly_linked_list::new();
+    d_list.push_front(3);
+    d_list.push_front(2);
+    d_list.push_front(1);
+    d_list.print_list();
+    // Pop values
+    println!("Popped: {:?}", d_list.pop_front());
+    d_list.print_list();
+
+    println!("Popped: {:?}", d_list.pop_front());
+    d_list.print_list();
 }
