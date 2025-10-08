7f1be6f0b06d341ced158491d529547e1ea008cc
jonandonigv <jonandonigv@gmail.com>
2025-10-08 14:08:30 +0200

    feature: Linked list

--- src/linked_list.rs
+++ src/linked_list.rs
@@ -1,15 +1,38 @@
+#[derive(Debug)]
 struct Node<T> {
     value: T,
     next: Option<Box<Node<T>>>,
 }
 
-struct Linked_list<T> {
+#[derive(Debug)]
+pub struct Linked_list<T> {
     head: Option<Box<Node<T>>>,
 }
 
 impl<T> Linked_list<T> {
-    fn new() -> Self {
+    // Create an empty list
+    pub fn new() -> Self {
         Linked_list { head: None }
     }
+
+    // Push a value to the front of the list
+    pub fn push(&mut self, value: T) {
+        let new_node = Box::new(Node {
+            value,
+            next: self.head.take(),
+        });
+        self.head = Some(new_node);
+    }
+
+    // Pop the front value
+    pub fn pop(&mut self) -> Option<T> {
+        self.head.take().map(|node| {
+            self.head = node.next;
+            node.value
+        })
+    }
+
+    pub fn peek(&self) -> Option<&T> {
+        self.head.as_ref().map(|node| &node.value)
+    }
 }
-pub fn linked_list() {}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,20 @@
-use dsa::add;
+use dsa::{add, linked_list::Linked_list};
 
 fn main() {
+    // Linked list
+    let mut list: Linked_list<i32> = dsa::linked_list::Linked_list::new();
+    // Push some values
+    list.push(1);
+    list.push(2);
+    list.push(3);
+    println!("List: {:?}", list);
+    // Peek at the head
+    println!("Peek: {:?}", list.peek());
+    // Pop values
+    println!("Popped: {:?}", list.pop());
+    println!("Popped: {:?}", list.pop());
+    println!("List after pops: {:?}", list);
+
     let result = add(2, 2);
     println!("Hello world!");
     println!("{result}");
