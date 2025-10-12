5fb44d2c796d186559ebb879a202118f7b7fa7b9
jonandonigv <jonandonigv@gmail.com>
2025-10-12 18:46:06 +0200

    feature: Linked list in golang

--- go/main.go
+++ go/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	linked_list "github.com/jonandonigv/DSA/list"
+)
 
 func main() {
-	fmt.Println("Hello wordl!")
+	l := linked_list.List{}
+	l.InsertAtTail(1)
+	l.InsertAtTail(1)
+	l.InsertAtHead(5)
+	l.InsertAtTail(1)
+
+	fmt.Println("Linked list:")
+	l.Print()
+
+	fmt.Println("Search 5:", l.Search(5))
+	l.DeleteByValue(1)
+	fmt.Println("After deleting 1:")
+	l.Print()
 }
