4fbc570947e0e6722519a93fc871adc0caf307b6
jonandonigv <jonandonigv@gmail.com>
2025-10-17 16:26:52 +0200

    wip: Queue list

--- go/queue/list_Q/list_queue.go
+++ go/queue/list_Q/list_queue.go
@@ -1,6 +1,9 @@
 package listq
 
-import "container/list"
+import (
+	"container/list"
+	"fmt"
+)
 
 type Queue struct {
 	list *list.List
@@ -8,4 +11,33 @@ type Queue struct {
 
 func NewQueue() *Queue {
 	return &Queue{list: list.New()}
+}
+
+func (q *Queue) Enqueue(item interface{}) {
+	q.list.PushBack(item)
+}
+
+func (q *Queue) Dequeue() (interface{}, error) {
+	if q.list.Len() == 0 {
+		return nil, fmt.Errorf("queue is empy")
+	}
+
+	e := q.list.Front()
+	q.list.Remove(e)
+	return e.Value, nil
+}
+
+func (q *Queue) Peek() (interface{}, error) {
+	if q.list.Len() == 0 {
+		return nil, fmt.Errorf("queue is empty")
+	}
+	return q.list.Front().Value, nil
+}
+
+func (q *Queue) IsEmpty() bool {
+	return q.list.Len() == 0
+}
+
+func (q *Queue) Size() int {
+	return q.list.Len()
 }
